b43667ef0e8ddf54c1335320f8492ca8336be0d0
Ayoub Ar <ayoubarahmat9@gmail.com>
2019-02-26 23:42:14 +0100

    debug

--- src/ecommerce/settings/local.py
+++ src/ecommerce/settings/local.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 SECRET_KEY = '@!-)jwmuzh8btr380g61=g+#&zzei&dz2(&=xbvxztady)_p(r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0', '68.183.169.89',"*"]
 
