7eadd7a99e7da6fdb667e9d84507fe9526bddb0d
Ayoub Ar <ayoubarahmat9@gmail.com>
2019-03-01 22:17:24 +0100

    ./

--- src/ecommerce/settings/local.py
+++ src/ecommerce/settings/local.py
@@ -32,7 +32,7 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'ABIRgd#@ay9581AY96G
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'Python ecommerce <ayoubarahmat9@gmail.com>'
-BASE_URL = 'https://www.bzoop.com/'
+BASE_URL = 'https://www.bzoop.com'
 
 MANAGERS = (
 	('Ayoub Arahmat', "ayoubarahmat9@gmail.com"),
